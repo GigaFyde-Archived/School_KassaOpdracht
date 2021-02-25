76916d567d3834a504292b224ff4d71b99aa54fd
Laurant Marijnissen <laurant@gigafyde.dev>
2021-02-25 13:42:25 +0100

    Partial rewrite and optimalizations

--- KassaOpdracht/Program.cs
+++ KassaOpdracht/Program.cs
@@ -7,39 +7,36 @@ namespace KassaOpdracht
     {
         static void Main(string[] args)
         {
-            var prices = new Dictionary<string, double>();
-            prices.Add("Tosti", 6.50);
-            prices.Add("Uitsmijter", 7.95);
-            prices.Add("Koffie", 2.55);
-            prices.Add("Melk", 2.00);
-            prices.Add("Frisdrank", 2.50);
+            var prices = new Dictionary<string, double>
+            {
+                { "Tosti", 6.50 },
+                { "Uitsmijter", 7.95 },
+                { "Koffie", 2.55 },
+                { "Melk", 2.00 },
+                { "Frisdrank", 2.50 }
+            };
 
             double amountDue = 0.00;
 
             Console.WriteLine("Vul het aantal Tosti's in:");
-            string s_amountOfTostis = Console.ReadLine();
-            int i_amountOfTostis = int.Parse(s_amountOfTostis);
-            amountDue += prices["Tosti"] * i_amountOfTostis;
+            int amountOfTostis = int.Parse(Console.ReadLine());
+            amountDue += prices["Tosti"] * amountOfTostis;
 
             Console.WriteLine("Vul het aantal Uitsmijters in:");
-            string s_amountOfUitsmijters = Console.ReadLine();
-            int i_amountOfUitsmijters = int.Parse(s_amountOfUitsmijters);
-            amountDue += prices["Uitsmijter"] * i_amountOfUitsmijters;
+            int amountOfUitsmijters = int.Parse(Console.ReadLine());
+            amountDue += prices["Uitsmijter"] * amountOfUitsmijters;
 
             Console.WriteLine("Vul het aantal koffie in:");
-            string s_amountOfCoffee = Console.ReadLine();
-            int i_amountOfCoffee = int.Parse(s_amountOfCoffee);
-            amountDue += prices["Koffie"] * i_amountOfCoffee;
+            int amountOfCoffee = int.Parse(Console.ReadLine());
+            amountDue += prices["Koffie"] * amountOfCoffee;
 
             Console.WriteLine("Vul het aantal Melk in");
-            string s_amountOfMilk = Console.ReadLine();
-            int i_amountOfMilk = int.Parse(s_amountOfMilk);
-            amountDue += prices["Melk"] * i_amountOfMilk;
+            int amountOfMilk = int.Parse(Console.ReadLine());
+            amountDue += prices["Melk"] * amountOfMilk;
 
             Console.WriteLine("Vul het aantal Frisdrank in");
-            string s_amountOfSoda = Console.ReadLine();
-            int i_amountOfSoda = int.Parse(s_amountOfSoda);
-            amountDue += prices["Frisdrank"] * i_amountOfSoda;
+            int amountOfSoda = int.Parse(Console.ReadLine());
+            amountDue += prices["Frisdrank"] * amountOfSoda;
             
             Console.WriteLine("Totaal prijs is {0} euro", amountDue);
          }
